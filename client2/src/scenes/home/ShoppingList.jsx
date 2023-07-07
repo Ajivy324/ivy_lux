ec113150a6ebcff95dcda2d02a6394d872c21396
Andrew Ivy <ajivy324@yahoo.com>
2023-07-06 20:58:53 -0500

    making edits to ShoppingList.jsx

--- client2/src/scenes/home/ShoppingList.jsx
+++ client2/src/scenes/home/ShoppingList.jsx
@@ -1,9 +1,26 @@
 import React, {useEffect, useState} from 'react'
-import Tabs from "@mui/material/Tabs";
+import { useDispatch, useSelector } from 'react-redux';
+import { Box, Typography, Tab, Tabs, useMediaQuery} from "@mui/material";
+import item from "../../components/Item";
+import { setItems } from "../../state";
+
 
 const ShoppingList = () => {
+    const dispatch = useDispatch();
+    const { value, setValue } = useState("all");
+    const items = useSelector((state) => state.cart.items);
+    const isNonMobile = useMediaQuery("(min-width:600px)");
+    console.log("items", items);
 
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
+    async function getItems() {
+        const items = await fetch(
+            "http://localhost:1337/api/items?populate=image"
+        );
+    }
     return (
     <div>ShoppingList</div>
     )
