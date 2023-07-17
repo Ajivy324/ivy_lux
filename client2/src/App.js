f6c3b4ec96cf6fc349c30e506932facf232fc0c2
Andrew Ivy <ajivy324@yahoo.com>
2023-07-16 22:14:08 -0500

    starting footer

--- client2/src/App.js
+++ client2/src/App.js
@@ -33,6 +33,7 @@ function App() {
           <Route path="checkout/success" element={<Confirmation />} />
         </Routes>
         <CartMenu />
+        <Footer />
       </BrowserRouter>
     </div> 
   );
