d7792ea7cf5b494e7035b7b27085986bb84b2001
Andrew Ivy <ajivy324@yahoo.com>
2023-07-19 22:31:48 -0500

    adding links to footer

--- client2/src/scenes/global/Footer.jsx
+++ client2/src/scenes/global/Footer.jsx
@@ -18,7 +18,27 @@ const Footer = () => {
                 columnGap="clamp(20px, 30px, 40px)"
             >
                 <Box width="clamp(20%, 30%, 40%)">
+                    <Typography 
+                    variant="h4" 
+                    fontWeight="bold" 
+                    mb="30px" 
+                    color={shades.secondary[500]}
+                    >
+                        Ivy Lux
+                    </Typography>
+                    <div>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </div>
+                </Box>
 
+                <Box>
+                    <Typography variant="h4" fontWeight="bold" mb="30px">
+                        About Us
+                    </Typography>
+                    <Typography mb="30px">Careers</Typography>
+                    <Typography mb="30px">Our Stores</Typography>
+                    <Typography mb="30px">Terms & Conditions</Typography>
+                    <Typography mb="30px">Privacy Policy</Typography>
                 </Box>
             </Box>
         </Box>
