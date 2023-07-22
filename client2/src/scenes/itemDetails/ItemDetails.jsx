8e9d7f81f6db4f28d9fde5e1a3dc7d83f7a84b8b
Andrew Ivy <ajivy324@yahoo.com>
2023-07-21 23:17:00 -0500

    starting  on item details page

--- client2/src/scenes/itemDetails/ItemDetails.jsx
+++ client2/src/scenes/itemDetails/ItemDetails.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+import {IconButton, Box, Typography, Button,  Tabs, Tab } from "@mui/material"
+import { FavoriteBorderOutlinedIcon } from '@mui/icons-material/FavoriteBorderOutlined';
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import {shades} from "../../theme";
+import { addToCart } from "../../state";
+import { useNParams } from "react-router-dom";
+
 
 const ItemDetails = () => {
 
+    const dispatch = useDispatch();
+    const {itemId} = useNParams();
 
     return (
         <div>ItemDetails</div>
