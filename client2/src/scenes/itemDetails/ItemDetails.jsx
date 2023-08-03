5c6a06dd53b8afa0b0e091604e31482301a7b402
Andrew Ivy <ajivy324@yahoo.com>
2023-08-02 22:27:45 -0500

    added a new line to ItemDetails.jsx

--- client2/src/scenes/itemDetails/ItemDetails.jsx
+++ client2/src/scenes/itemDetails/ItemDetails.jsx
@@ -68,9 +68,11 @@ const ItemDetails = () => {
                 </Box>
 
                 <Box m="65px 0 25px 0">
-                    <Typography></Typography>
-                    <Typography></Typography>
-                    <Typography></Typography>
+                    <Typography variant="h3">{item?.attributes?.name}</Typography>
+                    <Typography>${item?.attributes?.price}</Typography>
+                    <Typography sx={{ mt: "20px"}}>
+                        {item?.attributes?.longDescription}
+                    </Typography>
                 </Box>
             </Box>
         </Box>
