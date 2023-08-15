f7bae9a2c36f8ac9b07ae38f4d4f29f514be281b
Andrew Ivy <ajivy324@yahoo.com>
2023-08-14 22:27:19 -0500

    add imports to checkout page

--- client2/src/scenes/checkout/Checkout.jsx
+++ client2/src/scenes/checkout/Checkout.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { useSelector } from "react-redux"
+import  {Box, Button, Stopper, Step, StepLabel} from "@mui/material"
+import { Formik } from "formik"
+import { useState } from "react"
+import * as yup from "yup"
+import { shades } from "../../theme";
 
 const Checkout = () => {
-
+    const [activeStep, setActiveStep] = useState(0)
 
     return (
 
