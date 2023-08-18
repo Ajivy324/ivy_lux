18ad2b671ab38ef08f12778e180472c0b2441b6d
Andrew Ivy <ajivy324@yahoo.com>
2023-08-17 21:28:04 -0500

    addeed stepper to component

--- client2/src/scenes/checkout/Checkout.jsx
+++ client2/src/scenes/checkout/Checkout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector } from "react-redux"
-import  {Box, Button, Stopper, Step, StepLabel} from "@mui/material"
+import  {Box, Button, Stepper, Step, StepLabel} from "@mui/material"
 import { Formik } from "formik"
 import { useState } from "react"
 import * as yup from "yup"
@@ -9,11 +9,29 @@ import { shades } from "../../theme";
 const Checkout = () => {
     const [activeStep, setActiveStep] = useState(0)
     const cart = useSelector((state) => state.cart.cart);
+    const isFirstSteep = activeStep === 0;
+    const isSecondStep = activeStep === 1;
 
-    return (
+    const handleFormSubmit = async (value, actions) => {
+        setActiveStep(activeStep + 1);
+    }
 
-        <div>Checkout</div>
-    )
-}
+    async function makePayment(values){
+
+    }
+    return <Box width="80%" m="100px auto">
+        <Stepper activeStep={activeStep} sx={{m: "20px 0"}}>
+            <Step>
+                <StepLabel>Billing</StepLabel>
+            </Step>
+            <Step>
+                <StepLabel>Payment</StepLabel>
+            </Step>
+        </Stepper>
+        <Box>
+            
+        </Box>
+    </Box>
+};
 
 export default Checkout
