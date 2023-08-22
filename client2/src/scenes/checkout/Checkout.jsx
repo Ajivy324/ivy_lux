b06a37d59acc23ed93b25b1e17ab96fc4deee0e2
Andrew Ivy <ajivy324@yahoo.com>
2023-08-21 23:23:40 -0500

    completing checkout page

--- client2/src/scenes/checkout/Checkout.jsx
+++ client2/src/scenes/checkout/Checkout.jsx
@@ -16,8 +16,36 @@ const initialValues = {
         city: "",
         state: "",
         zipCode: "",
-    }
+    },
+    shippingAddress: {
+        isSameAddress: true,
+        firstName: "",
+        lastName: "",
+        county: "",
+        street1: "",
+        street2: "",
+        city: "",
+        state: "",
+        zipCode: "",
+    },
+    email: "",
+    phoneNumber:""
 }
+
+const checkoutSchema = [
+    yup.object().shape({
+        billingAddress: yup.object().shape({
+            firstName: yup.string().required("required"),
+            lastName: yup.string().required("required"),
+            county: yup.string().required("required"),
+            street1: yup.string().required("required"),
+            street2: yup.string(),
+            city: yup.string().required("required"),
+            state: yup.string().required("required"),
+            zipCode: yup.string().required("required"),
+        })
+    })
+]
 
 const Checkout = () => {
     const [activeStep, setActiveStep] = useState(0)
