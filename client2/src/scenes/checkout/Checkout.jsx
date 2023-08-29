8cebae0882011b3da68d1c5dab4d514207261ac2
Andrew Ivy <ajivy324@yahoo.com>
2023-08-28 21:56:41 -0500

    working on validations on checkout page

--- client2/src/scenes/checkout/Checkout.jsx
+++ client2/src/scenes/checkout/Checkout.jsx
@@ -43,6 +43,20 @@ const checkoutSchema = [
             city: yup.string().required("required"),
             state: yup.string().required("required"),
             zipCode: yup.string().required("required"),
+        }),
+        shippingAddress: yup.object().shape({
+            isSameAddress: yup.boolean(),
+            firstName: yup.string().when("isSameAddre3ss", {
+                is: false,
+                then: yup.string().required("required")
+            },
+            lastName: yup.string().required("required"),
+            county: yup.string().required("required"),
+            street1: yup.string().required("required"),
+            street2: yup.string(),
+            city: yup.string().required("required"),
+            state: yup.string().required("required"),
+            zipCode: yup.string().required("required"),
         })
     })
 ]
