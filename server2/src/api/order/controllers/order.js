b22f569e8dc97ffc16455825bcc065052ed58ab0
Andrew Ivy <ajivy324@yahoo.com>
2023-09-20 18:49:47 -0500

    added strapi to back end

--- server2/src/api/order/controllers/order.js
+++ server2/src/api/order/controllers/order.js
@@ -1,9 +1,26 @@
 'use strict';
-
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 /**
  * order controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController('api::order.order',({ strapi }) => ({
+    async create(ctx){
+        const { products, userName, email } = ctx.request.body;
+
+        try{
+            // retrieve item information
+            const lineItems = await Promise.all
+            products.map(async(product) => {
+                const item =  await  strapi
+                .service("api::item.item")
+                .findOne(product.id);
+            })
+
+        } catch (error){
+
+        }
+    }
+}));
